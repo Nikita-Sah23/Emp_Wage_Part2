91a4c4aa90d7470caac648371e9ef7c4e9fede51
Nikita-sah <nikitasah4gmail.com>
2022-08-27 12:17:01 +0530

    First commit

---
+++ EmployeeDailyWage.java
@@ -0,0 +1,35 @@
+package com.employeeWagePart2;
+
+public class EmployeeDailyWage {
+	
+	public void empWage() 
+	{
+	 int IS_PRESENT = 1;
+	 int WAGE_PER_HOUR = 20;
+	 int FULL_TIME = 8;
+	 int empWage = 0;
+
+	    int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+	System.out.println("empCheck=> "+empCheck);
+	if (empCheck == IS_PRESENT)
+	{
+	   System.out.println("Employee is present");
+	   empWage = WAGE_PER_HOUR * FULL_TIME ;
+	}
+	else
+	{
+	   System.out.println("Employee is absent");
+	   empWage = 0;
+	}
+	System.out.println("Employee wage + " +empWage);
+	}
+
+	
+	public static void main(String[] args) 
+	{
+		EmployeeDailyWage a = new EmployeeDailyWage ();
+	     a.empWage();
+
+	}
+
+}
